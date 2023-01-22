389d4526f6fee3cf04a2bd2bc49cc9f4f164a5bd
Sakil Anwar <sakilanwar60@gmail.com>
2023-01-23 00:10:09 +0600

    platform page added

--- js/main.js
+++ js/main.js
@@ -22,8 +22,3 @@ closeBtn.addEventListener('click', function () {
 
 
 // preloader
-
-// window.addEventListener('load', function () {
-//     document.querySelector('#loader-wrapper').classList.add("loaded")
-// });
-
