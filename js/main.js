856d85ab41f89e58fb140f09e16782dcb8525c66
Sakil Anwar <sakilanwar60@gmail.com>
2023-01-25 00:46:50 +0600

    platform bannar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 
+var swiper = new Swiper(".brands-slider", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+    speed: 1500,
+    breakpoints: {
+       480: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+        1200: {
+            slidesPerView: 6,
+            spaceBetween: 50,
+        },
+    },
+});
+
+
+
 
     (function ($) {
         "use strict";
