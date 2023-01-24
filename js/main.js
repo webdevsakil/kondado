ae8dadd3eafd3d0ca2154323128dd1546e5931ca
Sakil Anwar <sakilanwar60@gmail.com>
2023-01-24 12:33:50 +0600

    home page added

--- js/main.js
+++ js/main.js
@@ -1,24 +1,54 @@
 
 
+    (function ($) {
+        "use strict";
 
-// header
+        $(document).ready(function ($) {
 
-const openMenuBtn = document.querySelector('.header-area .open-menu-btn button');
-const mobileMenu = document.querySelector('.header-area .mobile-menu-area');
-const closeBtn = document.querySelector('.header-area .close-btn button');
-const body = document.querySelector('body');
-
-openMenuBtn.addEventListener('click', function () {
-    mobileMenu.classList.add('toggle-mobile-menu');
-    body.classList.add('bg-gray');
-})
-closeBtn.addEventListener('click', function () {
-    mobileMenu.classList.remove('toggle-mobile-menu');
-    body.classList.remove('bg-gray');
-})
+            $(".open-menu-btn").on('click', function () {
+                $('.mobile-menu-area').addClass('toggle-mobile-menu')
+            })
+            $(".close-btn").on('click', function () {
+                $('.mobile-menu-area').removeClass('toggle-mobile-menu')
+            })
 
 
 
+            // popup video
+
+            $('.video-player').magnificPopup({
+                type: 'iframe',
+                iframe: {
+                    markup: '<div class="mfp-iframe-scaler">' +
+                        '<div class="mfp-close"></div>' +
+                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                        '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+                    patterns: {
+                        youtube: {
+                            index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+                            id: 'v=',
+
+                            src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+                        },
+                        vimeo: {
+                            index: 'vimeo.com/',
+                            id: '/',
+                            src: 'https://player.vimeo.com/video/%id%?autoplay=1'
+                        },
+                        gmaps: {
+                            index: '//maps.google.',
+                            src: '%id%&output=embed'
+                        }
 
 
-// preloader
+                    },
+
+                    srcAction: 'iframe_src',
+                }
+                // other options
+            });
+
+        });
+    })(jQuery);
