c054e5b56f02fc794c30f9920e220574e31d1e05
Sakil Anwar <sakilanwar60@gmail.com>
2023-02-07 11:12:15 +0600

    header added

--- js/main.js
+++ js/main.js
@@ -27,17 +27,33 @@ var swiper = new Swiper(".brands-slider", {
 });
 
 
+// const mobileMenuArea = document.querySelectorAll('.mobile-menu-area .has-dropdown');
+// mobileMenuArea.forEach(item => {
+//   console.log(item)
+// })
+
+
 
 
 (function ($) {
   "use strict";
 
   $(document).ready(function ($) {
+
     $(".open-menu-btn").on("click", function () {
       $(".mobile-menu-area").addClass("toggle-mobile-menu");
     });
     $(".close-btn").on("click", function () {
       $(".mobile-menu-area").removeClass("toggle-mobile-menu");
+    });
+
+
+    const mobileMenuChildrens = $(".mobile-menu-area .has-dropdown");
+
+    Array.from(mobileMenuChildrens).forEach(function (hasSubmenu) {
+      $(hasSubmenu).on('click', function () {
+        $(this).toggleClass('submenuOpen');
+      });
     });
 
     // popup video
@@ -201,6 +217,14 @@ var swiper = new Swiper(".brands-slider", {
     });
 
     // sticky menu
-
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 20) {
+        $(".home-header-area").removeClass('transparent-header');
+        $(".home-header-area").addClass('header');
+      } else {
+        $(".home-header-area").removeClass('header');
+        $(".home-header-area").addClass('transparent-header');
+      }
+    });
   });
 })(jQuery);
