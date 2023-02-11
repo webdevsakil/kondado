39872f4c2a1d9858f2e7be1052954344b9016720
Sakil Anwar <sakilanwar60@gmail.com>
2023-02-11 13:33:04 +0600

    slider active

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-var swiper = new Swiper(".brands-slider", {
-  slidesPerView: 2,
-  spaceBetween: 20,
-  loop: true,
-  autoplay: {
-    delay: 1500,
-    disableOnInteraction: false,
-  },
-  speed: 1500,
-  breakpoints: {
-    480: {
-      slidesPerView: 3,
-    },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 5,
-      spaceBetween: 50,
-    },
-    1200: {
-      slidesPerView: 6,
-      spaceBetween: 50,
-    },
-  },
-});
 
 
 
@@ -49,9 +22,6 @@ var swiper = new Swiper(".brands-slider", {
         $(this).toggleClass('submenuOpen');
       });
     });
-
-
-
 
 
 
